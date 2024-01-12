0b044e7967ac3b89ef03c3f0e65c843f4a989be6
Brandocean <brandocean2016@gmail.com>
2024-01-12 16:36:26 -0600

    Jugar con otra version de React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+//! React 18
+// import React from 'react';
+// import ReactDOM from 'react-dom/';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+//! React 17
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+render(<App />, root);
+
+//? Aqui actualizamos en el package.json la version de react y react-dom de la 18 a la 17.0.2
